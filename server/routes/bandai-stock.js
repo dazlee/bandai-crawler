e2e9ec79274cd68a2ce90e031e49e21b485f43e0
Bryan Lee <daz.lee1987@gmail.com>
2018-05-01 12:11:12 +0800

    add

--- server/routes/bandai-stock.js
+++ server/routes/bandai-stock.js
@@ -7,6 +7,9 @@ const {
 	removeBandaiStockById,
 	getBandaiStockById
 } = require("../stores/bandai-stock");
+const csv = require("fast-csv");
+const fs = require("fs");
+const formidable = require("formidable");
 
 router.get("/", async function (req, res) {
 	const bandaiStocks = await getAllBandaiStocks();
@@ -29,6 +32,49 @@ router.post("/new", async function (req, res) {
 	attributes.createdBy = user.username;
 	await createBandaiStock(attributes);
 	res.redirect("/bandai-stock");
+});
+
+const headers = [
+	"name",
+	"url",
+];
+router.post("/new/csv", async function (req, res) {
+	try {
+		var form = new formidable.IncomingForm();
+		form.uploadDir = "./tmp";
+		form.parse(req, function(error, fields, files) {
+			if(error) {
+				console.log("error", error);
+				res.status(400).json(error.stack).end();
+				return;
+			}
+
+			const {user} = req;
+			const stream = fs.createReadStream(files.file.path);
+			let bandaiStocks = [];
+
+			csv.fromStream(stream, {headers})
+			.on("data", function(data){
+				try {
+					bandaiStocks.push(data);
+				} catch (error) {
+					console.log("error", error);
+					res.status(400).json(error.stack).end();
+				}
+			})
+			.on("end", async function() {
+				for (let i = 0, {length} = bandaiStocks; i < length; i++) {
+					let bandaiStock = bandaiStocks[i];
+					bandaiStock.notifyEmails = fields.notifyEmails;
+					bandaiStock.createdBy = user.username;
+					await createBandaiStock(bandaiStock);
+				}
+				res.redirect("/bandai-stock");
+			});
+		});
+	} catch (error) {
+		res.status(400).json(error.stack).end();
+	}
 });
 
 router.get("/:bandaiStockId/edit", async function (req, res) {
